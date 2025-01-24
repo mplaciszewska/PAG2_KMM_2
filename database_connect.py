707275897b245c9c52b736191e3455240b251c18
Maja Płaciszewska <100434543+mplaciszewska@users.noreply.github.com>
2025-01-24 11:18:02 +0100

    Update database_connect.py

--- database_connect.py
+++ database_connect.py
@@ -4,15 +4,15 @@ from pymongo.server_api import ServerApi
 
 def redis_connect():
     return redis.Redis(
-        host='redis-11605.c300.eu-central-1-1.ec2.redns.redis-cloud.com',
+        host='host',
         port=11605,
         decode_responses=True,
         username="default",
-        password="98T457YxhsCuPalg8mIaz1qIPKmgaudE",
+        password="password",
     )
 
 def mongodb_connect():
-    uri = "mongodb+srv://majakretstud:7Dq8XoLzmvl9X9Co@pag2-kmm.o1dnv.mongodb.net/?retryWrites=true&w=majority&appName=pag2-KMM"
+    uri = "uri"
     client = MongoClient(uri, server_api=ServerApi('1'))
     mongo_db = client.effacility
 
